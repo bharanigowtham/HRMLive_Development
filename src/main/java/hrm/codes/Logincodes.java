ecda761800c1569e4e6dee5c6774212b9b31d949
bharaniAuthor <bharani@gmail.com>
2025-09-04 19:48:36 +0800

    This is development code for login & submit claims flow.

---
+++ src/main/java/hrm/codes/Logincodes.java
@@ -0,0 +1,12 @@
+package hrm.codes;
+
+public class Logincodes {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is login page code.");
+		
+		
+	}
+
+}
---
+++ src/main/java/hrm/codes/SubmitClaimCode.java
@@ -0,0 +1,12 @@
+package hrm.codes;
+
+public class SubmitClaimCode {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is Submit claims page code.");
+		
+		
+	}
+
+}
