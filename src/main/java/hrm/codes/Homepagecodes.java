55d970b9a641e15a744844aba34a125c3dcf734d
bharani <bharani@gmail.com>
2025-09-08 19:25:13 +0800

    Home page code updated

--- src/main/java/hrm/codes/Homepagecodes.java
+++ src/main/java/hrm/codes/Homepagecodes.java
@@ -4,7 +4,7 @@ public class Homepagecodes {
 
 	public static void main(String[] args) {
 		
-		System.out.println("This is Homepagecodes class");
+		System.out.println("This is Homepagecodes class updated");
 		
 		
 	}
