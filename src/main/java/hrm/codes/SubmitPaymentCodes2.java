cbbf184cfbc1566f80c5433444c79ae340364670
bharanigowtham <bharanigowthamk@gmail.com>
2026-01-31 18:20:25 +0800

    New class created...

---
+++ src/main/java/hrm/codes/SubmitPaymentCodes2.java
@@ -0,0 +1,12 @@
+package hrm.codes;
+
+public class SubmitPaymentCodes2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is Submit Payment Codes 123");
+		
+		
+	}
+
+}
