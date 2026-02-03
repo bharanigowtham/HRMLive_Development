671941e24e23221976430bc10c798e3100c29f0a
bharanigowtham <bharanigowthamk@gmail.com>
2026-02-03 13:45:36 +0800

    Updated one

--- src/main/java/hrm/codes/SubmitForm_Core.java
+++ src/main/java/hrm/codes/SubmitForm_Core.java
@@ -3,7 +3,7 @@ package hrm.codes;
 public class SubmitForm_Core {
 
 	public static void main(String[] args) {
-		System.out.println("This is submit form code...");
+		System.out.println("This is submit form core...");
 	}
 
 }
